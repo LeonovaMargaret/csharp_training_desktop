eedfa96074c4f1aa4a159c5d46ecf31e97497f13
Маргарита Леонова <margo9861@gmail.com>
2020-12-13 09:29:55 +0200

    18 HW: Windows application - Group Creation and Removal Tests

---
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetList();
+
+            int removalIndex = 1;
+            app.Groups.Remove(removalIndex);
+
+            List<GroupData> newGroups = app.Groups.GetList();
+            oldGroups.RemoveAt(removalIndex);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
